76eba7b099ac3b0826b8eabf3d43486aedf8c1d7
CQFitz <nnabilmuqri123@gmail.com>
2021-09-23 09:15:04 +0000

    change '_' in link to '-'

--- app/routes/planned_workout.routes.js
+++ app/routes/planned_workout.routes.js
@@ -1,10 +1,10 @@
 module.exports = app => {
     const planned_workouts = require("../controllers/planned_workout.controller.js");
 
-    app.post("/planned_workouts", planned_workouts.create);
-    app.get("/planned_workouts/", planned_workouts.findAll);
-    app.get("/planned_workouts/:planned_workoutId", planned_workouts.findOne);
-    app.put("/planned_workouts/:planned_workoutId", planned_workouts.update);
-    app.delete("/planned_workouts/:planned_workoutId", planned_workouts.delete);
-    app.delete("/planned_workouts", planned_workouts.deleteAll);
+    app.post("/planned-workouts", planned_workouts.create);
+    app.get("/planned-workouts/", planned_workouts.findAll);
+    app.get("/planned-workouts/:planned_workoutId", planned_workouts.findOne);
+    app.put("/planned-workouts/:planned_workoutId", planned_workouts.update);
+    app.delete("/planned-workouts/:planned_workoutId", planned_workouts.delete);
+    app.delete("/planned-workouts", planned_workouts.deleteAll);
 };
